488825e0c7d3f588a46a05ea6814581102531f46
Levan Velijanashvili <stichoza@gmail.com>
2014-11-02 05:33:30 +0400

    database

---
+++ database/migrations/2014_11_01_233800_create_emulsifiers_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEmulsifiersTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('emulsifiers', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->unsignedInteger('number')->index();
+			$table->char('index', 1)->index()->nullable();
+			$table->string('name', 300);
+			$table->string('name_q', 400);
+			$table->text('description');
+			$table->boolean('vegetarian_safe')->nullable();
+			$table->boolean('vegean_safe')->nullable();
+			$table->boolean('fasting_safe')->nullable();
+			$table->boolean('prohibited_local')->nullable();
+			$table->boolean('prohibited_global')->nullable();
+			$table->enum('islam_status', ['halal', 'mashbooh', 'haram'])->nullable();
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('emulsifiers');
+	}
+
+}
